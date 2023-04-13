29f60020ce248e0e30445272f1a8bc97ee5ec254
DaniC <danic@secture.com>
2023-04-13 13:55:45 +0200

    Scenes demo

---
+++ ios/CarScene.swift
@@ -0,0 +1,12 @@
+// ios/CarScene.swift
+import Foundation
+import CarPlay
+class CarSceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
+  func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
+                                  didConnect interfaceController: CPInterfaceController) {
+    RNCarPlay.connect(with: interfaceController, window: templateApplicationScene.carWindow);
+  }
+  func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene, didDisconnectInterfaceController interfaceController: CPInterfaceController) {
+    RNCarPlay.disconnect()
+  }
+}
